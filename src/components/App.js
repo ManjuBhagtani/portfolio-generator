bf5826556cc6b00aed5ed01b975732c792df193e
manju <manjubhagtani@gmail.com>
2022-01-06 17:28:50 +0530

    Implemented a bootstrap header component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Header from './Bootstrap/Header';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Header>Portfolio Generator</Header>
       </div>
     )
   }
