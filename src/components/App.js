50241cd5ab28f16797b5a20efdd05b7c8681ef85
manju <manjubhagtani@gmail.com>
2022-01-07 17:04:29 +0530

    Added toggler on the Header.

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,26 @@ class App extends Component {
   state={
     Dark: false
   };
+  toggleHeader=()=>{
+    this.setState({
+      Dark: !this.state.Dark,
+    })
+  };
+  
   render() {
     return (
       <div className="App">
-        <Header dark={this.state.Dark}>Portfolio Generator</Header>
+        <Header dark={this.state.Dark} className='mb-5 justify-content-center'>
+          Portfolio Generator
+          <button 
+            className="btn btn-sm btn-outline-primary ml-3 rounded-circle" 
+            onClick={this.toggleHeader}
+          >
+            <i 
+              className={"fa fa-" + (this.state.Dark ? "sun" : "moon") + "-o"}
+            ></i>
+          </button>
+        </Header>
       </div>
     )
   }
