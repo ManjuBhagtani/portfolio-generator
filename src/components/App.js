fbb4cf4f3717b9ed1521a02483bfb3a26a370c99
manju <manjubhagtani@gmail.com>
2022-01-09 22:36:51 +0530

    Added a complete real data for generating code.

--- src/components/App.js
+++ src/components/App.js
@@ -6,39 +6,62 @@ class App extends Component {
   state={
     Dark: true,
     FormData: {
-      FirstName:"",
-      LastName:"",
+      FirstName:"xyz",
+      LastName:"abc",
       Thubmnail:"",
-      URL:"",
-      Description:"",
-      Keywords:"",
-      Address:"",
-      Phone:"",
-      Email:"",
+      URL:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.warmoven.in%2Fpikachu-photo-cake-2.html&psig=AOvVaw0mAt1oOLcy0krLwUkSgiSX&ust=1641834124052000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKD5hPKSpfUCFQAAAAAdAAAAABAD",
+      Description:"Hello World!",
+      Keywords:"abc, xyz, Software Engineer",
+      Address:"Maharashtra, India",
+      Phone:"+91 9999999999",
+      Email:"xyzabcgmail.com",
       Experience: [
         {
-          Role: "",
-          Company: "",
-          Desc: "",
-          Start: "",
-          End: ""
+          Role: "Awesome Developer",
+          Company: "Google",
+          Desc: "Worked as an awesome developer at Google.",
+          Start: "Jan 2020",
+          End: "Dec 2020"
+        },
+        {
+          Role: "Fantastic Developer",
+          Company: "Microsoft",
+          Desc: "Workeing as a fantastic developer at Microsoft.",
+          Start: "Jan 2021",
+          End: "Present"
         }
       ],
       Education: [
         {
-          Degree: "",
-          Institute: "",
-          Desc: "",
-          Start: "",
-          End: ""
+          Degree: "MCA",
+          Institute: "PQR",
+          Desc: "Learnt DSA, DBMS, OS",
+          Start: "Aug 2017",
+          End: "Aug 2020"
         }
       ],
       Skills: {
-        Tools: [],
-        Workflow: []
+        Tools: ["html5", "css3", "react"],
+        Workflow: [
+          "Mobile-First, Responsive Design",
+          "Cross Browser Testing & Debugging",
+          "Cross Functional Teams",
+          "Agile Development & Scrum"
+        ]
       },
-      Interests: [],
-      Awards:[]
+      Interests: [
+        "Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.",
+        "When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world."
+      ],
+      Awards:[
+        "Google Analytics Certified Developer",
+        "Mobile Web Specialist - Google Certification",
+        "1 st Place - University of Colorado Boulder - Emerging Tech Competition 2009",
+        "1 st Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)",
+        "2 nd Place - University of Colorado Boulder - Emerging Tech Competition 2008",
+        "1 st Place - James Buchanan High School - Hackathon 2006",
+        "3 rd Place - James Buchanan High School - Hackathon 2005",
+      ]
     }
   };
   toggleHeader=()=>{
