45a4caaa1b477fd119759f78eb7557406e5e97a4
Manju Bhagtani <manjubhagtani@gmail.com>
2022-04-15 23:24:26 +0530

    Added Download functionality

--- src/components/GeneratePortfolio/GeneratePortfolio.js
+++ src/components/GeneratePortfolio/GeneratePortfolio.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react'
 import Split from "react-split";
 import Form from './Form';
 import Code from './Code';
+import he from "he";
 import UIConstants from '../Constants/UIConstants';
 class GeneratePortfolio extends Component {
     state = {
         selectedTemplate: parseInt(localStorage.getItem("TemplateID")),
         FormData: UIConstants.Templates[Math.floor(parseInt(localStorage.getItem("TemplateID"))/2)].find(template => template.TemplateID === parseInt(localStorage.getItem("TemplateID"))).FormData,
         FormDataDesc: UIConstants.Templates[Math.floor(parseInt(localStorage.getItem("TemplateID"))/2)].find(template => template.TemplateID === parseInt(localStorage.getItem("TemplateID"))).Desc,
+        fileDownloadUrl: null,
     }
+
+    download = () => {
+        let output = he.decode(
+        document.getElementsByClassName("codefile")[0].innerHTML
+        );
+        const blob = new Blob([output]);
+        const fileDownloadUrl = URL.createObjectURL(blob);
+        this.setState({ fileDownloadUrl: fileDownloadUrl }, () => {
+        this.doFileDownload.click();
+        URL.revokeObjectURL(fileDownloadUrl);
+        this.setState({ fileDownloadUrl: "" });
+        });
+    };
 
     handleChange = e => {
         this.setState({
@@ -33,6 +48,24 @@ class GeneratePortfolio extends Component {
                             Desc={this.state.FormDataDesc}
                             onChange={this.handleChange}
                         />
+                        <button
+                            className="btn btn-success"
+                            onClick={() => {
+                            this.download();
+                            }}
+                            disabled={this.state.PreviewMode}
+                            title="Go to the Code View to download."
+                         >
+                            Download
+                        </button>
+                        <a
+                            className="d-none"
+                            download={"portfolio.html"}
+                            href={this.state.fileDownloadUrl}
+                            ref={e => (this.doFileDownload = e)}
+                        >
+                            Download
+                        </a>
                     </div>
                     <div className="p-3">
                         <Code
