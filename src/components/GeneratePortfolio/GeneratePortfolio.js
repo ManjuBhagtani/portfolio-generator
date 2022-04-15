d058c32e642d00721a56088b534defcbe2a362f9
Manju Bhagtani <manjubhagtani@gmail.com>
2022-04-15 21:30:13 +0530

    Added Splitter to separate ot form and code

--- src/components/GeneratePortfolio/GeneratePortfolio.js
+++ src/components/GeneratePortfolio/GeneratePortfolio.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import Split from "react-split";
 
 class GeneratePortfolio extends Component {
     render() {
         return (
-            <div>GeneratePortfolio</div>
+            <div className="container-fluid">
+                <Split className="split">
+                    <div className="p-3">
+
+                    </div>
+                    <div className="p-3">
+                        
+                    </div>
+                </Split>
+            </div>
         )
     }
 }
