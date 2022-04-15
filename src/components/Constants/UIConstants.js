e3660c4e188c66e6385a035878f404166ca0ec76
Manju Bhagtani <manjubhagtani@gmail.com>
2022-04-15 19:12:13 +0530

    Added meta data abount templates in a constants file

---
+++ src/components/Constants/UIConstants.js
@@ -0,0 +1,134 @@
+const UIConstants = {
+    "Templates": 
+    [
+        [{
+            TemplateID: 0,
+            AltText: "Template 1 Clarence Taylor",
+            DemoLink: "https://manjubhagtani.github.io/portfolio-templates/Template1_ClarenceTaylor/Template1ClarenceTaylor.html",
+            FormData: {
+                FirstName: "",
+                LastName: "",
+                Thubmnail: "",
+                URL: "",
+                Description: "",
+                Keywords: "",
+                Address: "",
+                Phone: "",
+                Email: "",
+                Colour: "#bd5e38",
+                Socials: {
+                    Facebook: "xyz",
+                    WhatsApp: "xyz",
+                    Instagram: "xyz",
+                    Twitter: "xyz",
+                    LinkedIn: "xyz",
+                    GitHub: "xyz",
+                    StackOverflow: "xyz"
+                }
+            },
+            Desc: {
+                FullName: [
+                    "text",
+                    "Full Name",
+                    "This is your full name. This has been generated using your first and last names."
+                ],
+                FirstName: [
+                    "text",
+                    "First Name",
+                    "Please enter your first name or given name."
+                ],
+                LastName: ["text", "Last Name", "Please enter your last name or surname"],
+                Thubmnail: [
+                    "text",
+                    "Your Photo",
+                    "Please upload your photo in a CDN(Ex. Imgur) and paste the direct image URL here."
+                ],
+                URL: [
+                    "text",
+                    "Website link",
+                    "Please enter the URL of your website, where this page is going to be hosted"
+                ],
+                Description: ["text", "About you", "Write something nice about you."],
+                Keywords: [
+                    "text",
+                    "Keywords",
+                    "If someone wants to search for you, what keywords should they use?"
+                ],
+                Address: [
+                    "text",
+                    "Where are you?",
+                    "Type in your full address to get kidnapped!"
+                ],
+                Phone: [
+                    "text",
+                    "Phone Number",
+                    "Please enter your phone number, so we can call you in the midnight!"
+                ],
+                Email: ["text", "Email Address", "Tell us your primary email address"],
+                Colour: [
+                    "color",
+                    "Theme Colour",
+                    "Please choose the colour for your portfolio"
+                ]    
+            }     
+        },
+        {
+            TemplateID: 1,
+            AltText: "Template 2 Anna Peterson",
+            DemoLink: "https://manjubhagtani.github.io/portfolio-templates/Template2_AnnaPeterson/Template2AnnaPeterson.html",
+            FormData: {
+                FirstName: "",
+                LastName: "",
+                Thubmnail: "",
+                URL: "",
+                Description: "",
+                Keywords: "",
+                Address: "",
+                Phone: "",
+                Email: "",
+                Colour1: "#000",
+                Colour2: "#f393a4",
+                Colour3: "#362f47",
+                Colour4: "#7e5a7b"
+            }      
+        }],
+        [{
+            TemplateID: 2,
+            AltText: "Template 3 Kalvin Doe",
+            DemoLink: "https://manjubhagtani.github.io/portfolio-templates/Template4_KalvinDoe/Template4_KalvinDoe.html",
+            FormData: {
+                FirstName: "",
+                LastName: "",
+                Tagline: "",
+                "Background/Banner Image": "",
+                Thubmnail: "",
+                Keywords: "",
+                Description: "",  
+                "CV Link": "",             
+                Address: "",
+                Phone: "",
+                Email: "",
+            }       
+        },
+        {
+            TemplateID: 3,
+            AltText: "Template 4 Mark Parker",
+            DemoLink: "https://manjubhagtani.github.io/portfolio-templates/Template3_MarkParker/Template3MarkParker.html",
+            FormData: {
+                FirstName: "",
+                LastName: "",
+                Tagline: "",
+                Thubmnail: "",
+                Keywords: "",
+                Description: "",              
+                Address: "",
+                Phone: "",
+                Email: "",
+                Colour1: "#754ef9",
+                Colour2: "#152136",
+            }
+        }],
+    ]
+}
+
+export default UIConstants;
