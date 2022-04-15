428b4d2978984ab40492a913a83daddd581bbd03
Manju Bhagtani <manjubhagtani@gmail.com>
2022-04-15 23:57:28 +0530

    Added second template's Code component

--- src/components/GeneratePortfolio/Code.js
+++ src/components/GeneratePortfolio/Code.js
@@ -1,10 +1,12 @@
 import { connect } from "react-redux";
 import Template0 from "./PortfolioTemplates/Template0/Template0";
+import Template1 from "./PortfolioTemplates/Template1/Template1";
 
 const Code = ({FormData, FullName, TemplateID}) => {
   const renderTemplate = (templateID) =>{
     switch(templateID){
       case 0: return <Template0 FormData={FormData} FullName={FullName}/>
+      case 1: return <Template1 FormData={FormData} FullName={FullName}/>
       default:  return <Template0 FormData={FormData} FullName={FullName}/>
     }
   }
